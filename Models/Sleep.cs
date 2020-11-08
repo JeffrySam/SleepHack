c7293046971f2985d1a82dafb5523a66f31a7f25
Nabiha <nabihazaman26@gmail.com>
2020-11-08 02:05:30 -0600

    Updated Login pages

--- Models/Sleep.cs
+++ Models/Sleep.cs
@@ -9,6 +9,8 @@ namespace SleepHack.Models
     {
         public int ID { get; set;}
         public string Name { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
         public int Age { get; set; }
         public int Height{ get; set; }
         public int Weight { get; set; }
--- Pages/Login.cshtml.cs
+++ Pages/Login.cshtml.cs
@@ -4,13 +4,16 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using SleepHack.Data;
+using SleepHack.Models;
 
-namespace SleepHack.Pages.User
+namespace SleepHack.Pages
 {
     public class LoginModel : PageModel
     {
         public void OnGet()
         {
+            
         }
     }
 }
