d777addeab64827fdfd18fdd7bb9d20e7d0b69fa
Ryan Fung <rkfung88@gmail.com>
2020-11-07 21:54:29 -0600

    tt

--- Pages/Log.cshtml.cs
+++ Pages/Log.cshtml.cs
@@ -3,15 +3,40 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace SleepHack.Pages
 {
     public class LogModel : PageModel
     {
+        [BindProperty]
+        public DateTime sleep { get; set; }
+
+        [BindProperty]
+        public int sleeprate { get; set; }
+        
+        [BindProperty]
+        public string sleepnotes { get; set; }
+
+        [BindProperty]
+        public DateTime wake { get; set; }
+        
         public void OnGet()
         {
 
+        }
+
+        public IActionResult OnPost()
+        {
+            if(ModelState.IsValid == false)
+            {
+                return Page();
+            }
+
+            return RedirectToPage("/Index");
+            
         }
     }
 }
